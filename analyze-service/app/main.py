f543d97dc294c1f66a8239433110b363e228be3c
Moshe <moshe@example.com>
2026-03-17 18:25:25 +0200

    feat: handle ni mysql

--- analyze-service/app/main.py
+++ analyze-service/app/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from mysql_connection import connect
 from routes import router
+import time
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
-        conn_mysql = connect()
-        app.state.conn_mysql = connect()
+
+        retry = 10
+        while retry !=0:
+                conn_mysql = connect()
+                app.state.conn_mysql = conn_mysql
+                if conn_mysql:
+                        break
+                else:
+                        retry -= 1
+                        time.sleep(2)
         yield
         conn_mysql.close()
         
