f401ed8d996979db6eba98ea7326986af699feab
Moshe <moshe@example.com>
2026-03-17 17:21:22 +0200

    feat: adding endpoints

--- analyze-service/app/routes.py
+++ analyze-service/app/routes.py
@@ -1,10 +1,41 @@
-from fastapi import APIRouter,Request,Depends
+from fastapi import APIRouter,Request,Depends, HTTPException
 from mysql.connector import MySQLConnection
 from dal import *
+
+
 router = APIRouter()
 def get_connection(request:Request):
-    return request.app.stat.conn_mysql
+    return request.app.state.conn_mysql
 
-@router.get('/query_1',status_code=200)
-def query_1_res(conn: MySQLConnection = Depends(get_connection)):
-    return execute_query(conn,query_1())
+@router.get('/quality_targets_movement_alert',status_code=200)
+def quality_targets_alert(conn: MySQLConnection = Depends(get_connection)):
+    try:
+        return {'result':execute_query(conn,quality_targets_movement_alert())}
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=e)
+    
+
+@router.get('/process_intel_sources',status_code=200)
+def intel_sources(conn: MySQLConnection = Depends(get_connection)):
+    try:
+        return execute_query(conn,process_intel_sources())
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=e)
+    
+
+@router.get('/detection_new_targets',status_code=200)
+def new_targets(conn: MySQLConnection = Depends(get_connection)):
+    try:
+        return execute_query(conn,detection_new_targets())
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=e)
+    
+
+@router.get('/identification_strange_movement',status_code=200)
+def strange_movement(conn: MySQLConnection = Depends(get_connection)):
+    try:
+        return execute_query(conn,identification_strange_movement())
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=e)
+    
+
