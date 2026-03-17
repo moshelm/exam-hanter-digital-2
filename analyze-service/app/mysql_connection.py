4af2bc09c99e978d15489237f946474bcd61b4f0
Moshe <moshe@example.com>
2026-03-17 16:12:56 +0200

    feat: create connection to mysql

---
+++ analyze-service/app/mysql_connection.py
@@ -0,0 +1,14 @@
+import mysql.connector
+from mysql.connector import MySQLConnection
+import os 
+
+mysql_host = os.getenv("MYSQL_HOST",'mysql')
+mysql_port = os.getenv("MYSQL_PORT",'3306')
+mysql_user = os.getenv("MYSQL_USER",'root')
+mysql_password = os.getenv("MYSQL_PASSWORD",'root')
+
+def connect()->MySQLConnection:
+    client = mysql.connector.connect(host=mysql_host,port=mysql_port,user=mysql_user,
+                                     password=mysql_password)
+    return client
+    
